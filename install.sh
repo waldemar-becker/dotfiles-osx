820a73e18ebd7ddf02f74c695cd7140acb0f9c12
wbecker <waldemar88@web.de>
2021-07-29 20:29:43 +0200

    add graphical apps and python packages to install.sh

--- install.sh
+++ install.sh
@@ -11,22 +11,31 @@ brew install \
   bash-completion \
   bat \
   exa \
-  gdbm \
-  gettext \
   git \
   k9s \
   libzip \
-  md5sha1sum \
-  mpdecimal \
-  openssl@1.1 \
-  pcre \
-  pcre2 \
   python@3.9 \
-  readline \
-  sqlite \
   the_silver_searcher \
   tldr \
   tree \
-  xz \
   font-hack-nerd-font \
   postman
+
+# Install apps with UI
+brew install --cask \
+  rectangle \
+  iterm2 \
+  firefox \
+  google-chrome \
+  macpass \
+  spotify \
+  pycharm \
+  atom
+
+# Install pip package manager
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
+
+# Install useful python packages
+pip install -r requirements.txt
